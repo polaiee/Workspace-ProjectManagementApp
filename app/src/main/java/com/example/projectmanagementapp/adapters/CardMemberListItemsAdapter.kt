0519ffa2aba290febd719a20e7ca276747eee94f
polaiee <vapa18cs@cmrit.ac.in>
2021-09-05 00:35:01 +0530

    Create CardMemberListItemsAdapter.kt

---
+++ app/src/main/java/com/example/projectmanagementapp/adapters/CardMemberListItemsAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.projectmanagementapp.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.projectmanagementapp.R
+import com.example.projectmanagementapp.models.SelectedMembers
+import kotlinx.android.synthetic.main.item_card_selected_member.view.*
+
+open class CardMemberListItemsAdapter(
+    private val context:Context,
+    private val list: ArrayList<SelectedMembers>,
+    private val assignedMembers: Boolean
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var onClickListener : OnClickListener?= null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_card_selected_member,parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if(holder is MyViewHolder){
+            if(position == list.size-1 && assignedMembers){
+                holder.itemView.iv_add_member.visibility = View.VISIBLE
+                holder.itemView.iv_selected_member_image.visibility = View.GONE
+            }else{
+                holder.itemView.iv_add_member.visibility = View.GONE
+                holder.itemView.iv_selected_member_image.visibility = View.VISIBLE
+
+                Glide
+                    .with(context)
+                    .load(model.image)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(holder.itemView.iv_selected_member_image)
+            }
+
+            holder.itemView.setOnClickListener {
+                onClickListener?.onClick()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    fun setOnClickListener(onClickListener: OnClickListener){
+        this.onClickListener = onClickListener
+    }
+
+    interface OnClickListener{
+        fun onClick()
+    }
+    private class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+}
